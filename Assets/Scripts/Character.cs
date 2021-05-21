20b19f97f74760d5f32854285184fc6b43bcbab6
jon-intertwinedgames <jon@intertwinedgames.com>
2021-05-21 18:16:55 -0400

    Created Character scriptable object script.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : ScriptableObject
+{
+    public new string name;
+    public Sprite avatar;
+    public Sprite background;
+
+    public int maxHealth;
+    public int currentHealth;
+    public int armor;
+
+    public GameObject ability1;
+    public GameObject ability2;
+    public GameObject ability3;
+    public GameObject ability4;
+
+    /*Consider the following attributes later:
+     * type
+     * attack
+     * defense
+     * magic
+     */
+}
